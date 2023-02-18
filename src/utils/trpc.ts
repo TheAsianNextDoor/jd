914744999bdabbc7e2c4969e702624514a96165f
TheAsianNextDoor <aaron.orlando.scherling@gmail.com>
2023-02-18 12:15:59 -0600

    remove serverEnv

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -2,12 +2,12 @@ import { QueryClient } from "@tanstack/solid-query";
 import type { IAppRouter } from "~/server/trpc/router/_app";
 import { createTRPCSolid } from "solid-trpc";
 import { httpBatchLink } from "@trpc/client";
-import { serverEnv } from "~/env/server";
+// import { serverEnv } from "~/env/server";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return "";
   //   return `http://localhost:${process.env.PORT ?? 3000}`;
-  return process?.env?.VERCEL_URL || serverEnv.BASE_URL;
+  return process?.env?.VERCEL_URL;
 };
 
 export const trpc = createTRPCSolid<IAppRouter>();
